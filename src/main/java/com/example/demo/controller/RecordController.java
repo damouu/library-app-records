291f934c9ad11114b84539c20c0b55724bc7fa39
damouu <mouadsehbaoui@gmail.com>
2026-02-04 17:45:04 +0100

    Refactor: RecordController (#LAR-7)

--- src/main/java/com/example/demo/controller/RecordController.java
+++ src/main/java/com/example/demo/controller/RecordController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.RecordService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,24 +10,25 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 import java.util.UUID;
 
-@Data
 @Validated
-@CrossOrigin
 @RestController
-@RequestMapping("api/records")
+@RequiredArgsConstructor
 public class RecordController {
 
     private final RecordService recordService;
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping(path = "/membercard/{memberCardUUID}/history", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getHistory(@PathVariable UUID memberCardUUID, @RequestParam Map<String, ?> allParams, @AuthenticationPrincipal Jwt jwt) {
+    @GetMapping(path = "/api/records", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getHistory(@RequestHeader("X-User-UUID") UUID memberCardUUID, @RequestParam Map<String, ?> allParams, @AuthenticationPrincipal Jwt jwt) {
 
         String jwtMemberCard = jwt.getClaimAsString("user_memberCardUUID");
 
