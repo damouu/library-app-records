90b427b2c930836e227b551d6eb7880e7adb5492
damouu <mouadsehbaoui@gmail.com>
2026-06-14 20:11:06 +0900

    Refactor: KafkaPayloadBuilderServiceTest (#LAR-15)

--- src/test/java/com/example/demo/unit/service/KafkaPayloadBuilderServiceTest.java
+++ src/test/java/com/example/demo/unit/service/KafkaPayloadBuilderServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.demo.unit.service;
 
-import com.example.demo.dto.BorrowEventPayload;
-import com.example.demo.model.Record;
+import com.example.demo.dto.BorrowCreatedEvent;
+import com.example.demo.dto.ChapterCreatedEvent;
+import com.example.demo.mapper.BorrowRecordMapper;
+import com.example.demo.mapper.ChapterMapper;
+import com.example.demo.model.BorrowRecord;
+import com.example.demo.model.ChapterProjection;
 import com.example.demo.service.KafkaPayloadBuilderService;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
@@ -17,19 +21,53 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ExtendWith(MockitoExtension.class)
 class KafkaPayloadBuilderServiceTest {
 
-    private final KafkaPayloadBuilderService service = new KafkaPayloadBuilderService();
+    private final BorrowRecordMapper borrowRecordMapper = new BorrowRecordMapper();
+
+    private final ChapterMapper chapterMapper = new ChapterMapper();
+
+    private final KafkaPayloadBuilderService service = new KafkaPayloadBuilderService(chapterMapper, borrowRecordMapper);
 
 
     @Test
-    @DisplayName("Should listen to a borrowEventPayload")
+    @DisplayName("Should build BorrowRecord from BorrowCreatedEvent")
     void testBuildBorrowEntities() {
-        BorrowEventPayload borrowEventPayload = Instancio.create(BorrowEventPayload.class);
-        borrowEventPayload.getData().getNotificationData().setBorrowStartDate(LocalDate.now().toString());
-        borrowEventPayload.getData().getNotificationData().setBorrowEndDate(LocalDate.now().plusDays(5).toString());
-        Record record = service.buildBorrowEntities(borrowEventPayload);
-        assertEquals(borrowEventPayload.getData().getNotificationData().getBorrowUuid(), record.getBorrowUuid());
-        assertEquals(borrowEventPayload.getMetadata().getMemberCardUUID(), record.getMemberCardUuid());
-        assertNotNull(record.getBooks());
-        assertNotNull(record.getChapters());
+
+        BorrowCreatedEvent borrowCreatedEvent = Instancio.create(BorrowCreatedEvent.class);
+
+        borrowCreatedEvent.getData().setBorrow_start_date(LocalDate.now().toString());
+
+        borrowCreatedEvent.getData().setBorrow_end_date(LocalDate.now().plusDays(5).toString());
+
+        BorrowRecord borrowRecord = service.buildBorrowEntities(borrowCreatedEvent);
+
+        assertEquals(borrowCreatedEvent.getData().getBorrow_uuid(), borrowRecord.getBorrowUuid());
+
+        assertEquals(borrowCreatedEvent.getData().getMember_card_uuid(), borrowRecord.getMemberCardUuid());
+
+        assertNotNull(borrowRecord.getBorrowStartDate());
+        assertNotNull(borrowRecord.getBorrowEndDate());
+
+        assertEquals(null, borrowRecord.getReturnLately());
+        assertEquals(null, borrowRecord.getDaysLate());
+    }
+
+
+    @Test
+    @DisplayName("Should build ChapterProjection from ChapterCreatedEvent")
+    void testBuildChapterEntities() {
+
+        ChapterCreatedEvent event = Instancio.create(ChapterCreatedEvent.class);
+
+        event.getData().setPublication_date(LocalDate.now().toString());
+
+        ChapterProjection result = service.buildChapterEntities(event);
+
+        assertNotNull(result);
+
+        assertEquals(event.getData().getChapter_uuid(), result.getChapterUuid());
+
+        assertEquals(event.getData().getTitle(), result.getTitle());
+
+        assertEquals(event.getData().getChapter_number(), result.getChapterNumber());
     }
 }
