5f0987cb19e26bf59dd0962291d3037e57bd82cc
damouu <mouadsehbaoui@gmail.com>
2026-06-21 18:33:05 +0900

    Refactor: BorrowCreatedEventData (#LAR-19)

--- src/main/java/com/example/demo/dto/BorrowCreatedEventData.java
+++ src/main/java/com/example/demo/dto/BorrowCreatedEventData.java
@@ -1,27 +1,20 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class BorrowCreatedEventData {
+public record BorrowCreatedEventData(
+        UUID member_card_uuid,
 
-    private UUID member_card_uuid;
+        UUID borrow_uuid,
 
-    private UUID borrow_uuid;
+        String borrow_start_date,
 
-    private String borrow_start_date;
+        String borrow_end_date,
 
-    private String borrow_end_date;
-
-    private List<BorrowedItem> borrowed_items;
-
+        List<BorrowedItem> borrowed_items
+) {
 }
