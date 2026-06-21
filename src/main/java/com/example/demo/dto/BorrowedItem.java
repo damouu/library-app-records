0f4231cea5da26b410e598b91b70c7cb6a769215
damouu <mouadsehbaoui@gmail.com>
2026-06-21 18:33:20 +0900

    Refactor: BorrowedItem (#LAR-19)

--- src/main/java/com/example/demo/dto/BorrowedItem.java
+++ src/main/java/com/example/demo/dto/BorrowedItem.java
@@ -1,19 +1,12 @@
 package com.example.demo.dto;
 
 
-import lombok.*;
-
 import java.util.UUID;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class BorrowedItem {
 
-    private UUID book_uuid;
+public record BorrowedItem(
+        UUID book_uuid,
 
-    private UUID chapter_uuid;
-    
+        UUID chapter_uuid
+) {
 }
