e4e7c877b8eb4f02aa20ae8ee536d31a9e52b960
damouu <mouadsehbaoui@gmail.com>
2026-06-21 18:36:18 +0900

    Refactor: ReturnCreatedEventData (#LAR-19)

--- src/main/java/com/example/demo/dto/ReturnCreatedEventData.java
+++ src/main/java/com/example/demo/dto/ReturnCreatedEventData.java
@@ -2,37 +2,31 @@ package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ReturnCreatedEventData {
+public record ReturnCreatedEventData(
 
-    private UUID member_card_uuid;
+        UUID member_card_uuid,
 
-    private UUID borrow_uuid;
+        UUID borrow_uuid,
 
-    private String borrow_start_date;
+        String borrow_start_date,
 
-    private String borrow_end_date;
+        String borrow_end_date,
 
-    private String borrow_return_date;
+        String borrow_return_date,
 
-    private boolean return_lately;
+        boolean return_lately,
 
-    private int days_late;
+        int days_late,
 
-    private BigDecimal late_fee;
+        BigDecimal late_fee,
 
-    @JsonProperty("returned_items")
-    private List<BookToDecrement> returned_items;
-
+        @JsonProperty("returned_items")
+        List<BookToDecrement> returned_items
+) {
 }
